329b08a536f8f6e41c50593e3ee98bc585a0cfc9
yuichiro12 <talcnrhyme@gmail.com>
2017-02-22 01:02:07 +0900

    finish static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+
+  get 'foo/baz'
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
   root 'application#hello'
 end

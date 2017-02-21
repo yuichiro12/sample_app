9515ab449ce5bb715f2ab4101deb73dd6e3b4e95
yuichiro12 <talcnrhyme@gmail.com>
2017-02-22 08:16:34 +0900

    finish layout and routing

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.1'
+# bootstrap
+gem 'bootstrap-sass', '~> 3.3.6'
 # Use mysql as the database for Active Record
 gem 'mysql2', '>= 0.3.18', '< 0.5'
 # Use Puma as the app server
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
+  root 'static_pages#home'
 
-  get 'foo/baz'
+  # get "/help", to: "static_pages#help" as: "helf"
+  get "/help", to: "static_pages#help"
 
-  get 'static_pages/home'
+  get "/about", to: "static_pages#about"
 
-  get 'static_pages/help'
+  get "/contact", to: "static_pages#contact"
 
-  get 'static_pages/about'
-
-  root 'application#hello'
+  get "/login", to: "static_pages#login"
+  
+  get "/signup", to: "users#new"
 end
